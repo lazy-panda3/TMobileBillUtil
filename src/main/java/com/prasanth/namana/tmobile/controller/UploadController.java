88096cee1241f52f73c3d9574bfc2e6ab498d89d
lazy-panda3 <learntocode369@gmail.com>
2021-12-07 19:56:07 -0600

    Dono

--- src/main/java/com/prasanth/namana/tmobile/controller/UploadController.java
+++ src/main/java/com/prasanth/namana/tmobile/controller/UploadController.java
@@ -9,26 +9,27 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @Controller
+@RequestMapping("/")
 public class UploadController {
 
     @Autowired
     public UploadService uploadService;
 
     @ResponseBody
-    @PostMapping("/upload")
+    @PostMapping("upload")
     public String uploadFile(@RequestParam("file") MultipartFile multipartFile) throws IOException {
 
        // MultipartFile multipartFile = (MultipartFile) request.getAttribute("file");
         return uploadService.processFile(multipartFile);
     }
-    @RequestMapping("/")
+    @RequestMapping(method = RequestMethod.GET)
     public String index(){
         return "forward:/uploadFile.html";
     }
 
 
     @ResponseBody
-    @GetMapping("/uploady")
+    @GetMapping("uploady")
     public  String uploadFile(){
         return "This is a test";
     }
